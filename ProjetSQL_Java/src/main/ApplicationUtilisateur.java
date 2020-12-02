ba5f7ac1a5a180ebbb3489bc3899c7d06fbdec23
Baptiste Honnay <honnaybaptiste@gmail.com>
2020-12-02 19:07:40 +0100

    add - user app skeleton

--- ProjetSQL_Java/src/main/ApplicationUtilisateur.java
+++ ProjetSQL_Java/src/main/ApplicationUtilisateur.java
@@ -1,10 +1,72 @@
 package main;
 
+import db.Db;
+import utils.Utils;
+
 public class ApplicationUtilisateur {
-
+	private static Db db = new Db();
+	private static final String[] MENU_OPTIONS = {
+			"Afficher les examens",
+			"S'inscrire à un examen",
+			"S'inscrire a tous les examens du bloc",
+			"Afficher son horaire d'examen",
+			"Se deconnecter",
+			"Quitter"
+	};
+	
 	public static void main(String[] args) {
-		
-
+		boolean quit = false;
+		while(!quit) { 
+			String username = connect();
+			boolean connected = true;
+			while(connected && !quit) {
+				Utils.displayMainMenu(MENU_OPTIONS);
+				int choice = Utils.inputStrictlyPositiveIntegerLE(MENU_OPTIONS.length);
+				
+				switch(choice) {
+				case 1:
+					displayExams();
+					break;
+				case 2:
+					subscribeExam();
+					break;
+				case 3:
+					subscribeAll();
+					break;
+				case 4:
+					displaySchedule();
+					break;
+				case 5:
+					connected = false;
+					break;
+				default:
+					quit = true;
+				}
+			}
+		}
 	}
 
+	private static void displayExams() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void subscribeExam() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void subscribeAll() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private static void displaySchedule() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static String connect() {
+		return "";
+	}
 }
